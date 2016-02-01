97c7368b34fb41e2e0d1587a3b5187dad9a5f660
Patrick Woolfenden <hockeywoolf19@hotmail.com>
2016-01-31 23:44:34 -0600

    Firmware and STL updates

--- Firmware/Marlin-1.0.2-Printer-Kitten/Marlin/Configuration.h
+++ Firmware/Marlin-1.0.2-Printer-Kitten/Marlin/Configuration.h
@@ -526,8 +526,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 //#define EEPROM_CHITCHAT
 
 // Preheat Constants
-#define PLA_PREHEAT_HOTEND_TEMP 215
-#define PLA_PREHEAT_HPB_TEMP 70
+#define PLA_PREHEAT_HOTEND_TEMP 210
+#define PLA_PREHEAT_HPB_TEMP 50
 #define PLA_PREHEAT_FAN_SPEED 101   // Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 240
